adf763131af341a4cb9924703805d00c6462c895
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2024-01-21 20:30:58 +0200

    fix

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -42,10 +42,7 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
     try {
-        const { error } = createContactSchema.validate(req.body);
-        if (error) {
-            throw HttpError(400, error.message)
-        }
+
         const result = await contactsService.addContact(req.body);
         res.status(201).json(result)
 
@@ -56,10 +53,6 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
     try {
-        const { error } = updateContactSchema.validate(req.body);
-        if (error) {
-            throw HttpError(400, "Body must have at least one field" )
-        }
         const { id } = req.params;
         const result = await contactsService.updateContact(id, req.body);
         if (!result) {
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -6,6 +6,11 @@ import {
   createContact,
   updateContact,
 } from "../controllers/contactsControllers.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import validateBody from "../middlewares/validateBody.js";
 
 const contactsRouter = express.Router();
 
@@ -15,8 +20,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
 
 export default contactsRouter;
